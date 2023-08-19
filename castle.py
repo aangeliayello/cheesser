52ad401763738040efecd37c7d94b9af0c70ee1b
aangeliayello <aloangeli@gmail.com>
2023-08-19 14:45:50 +0200

    castle testing

--- castle.py
+++ castle.py
@@ -1,7 +1,43 @@
 import numpy as np
 from board import Board
-from classes import Color
-from moves import get_best_moveAB
+from classes import Color, Piece
+from moves import get_best_moveAB, get_legal_moves, get_legal_moves_from
+from utils import  get_right_bit_index
+
+pboard = [['r' '_' 'b' 'q' 'k' '_' '_' 'r'],
+ ['p' 'p' 'p' 'p' '_' 'p' 'p' 'p'],
+ ['_' '_' '_' '_' 'p' 'n' '_' '_'],
+ ['_' '_' '_' '_' '_' '_' '_' '_'],
+ ['_' 'n' '_' 'P' '_' '_' '_' '_'],
+ ['_' '_' '_' 'B' 'P' '_' '_' '_'],
+ ['P' 'P' 'P' 'K' '_' 'P' 'P' 'P'],
+ ['R' 'N' '_' 'Q' '_' '_' 'N' 'R']]
+
+
+
+
+print('Castle KingSide White')
+color_to_play = Color.BLACK
+castling_available = np.array([[True, True],
+                            [True, True]])
+board = Board()
+board.from_printed_board(pboard, color_to_play)
+board.castling_available = castling_available
+piece = Piece.KING
+from_ = get_right_bit_index(board.pieces[board.color_to_play][Piece.KING])
+king_moves = get_legal_moves_from(Piece.KING, board, from_)
+for m in king_moves:
+    print(m)
+
+castling_king_side_white1, _ = get_best_moveAB(board, 1)
+print(castling_king_side_white1)
+castling_king_side_white2, _ = get_best_moveAB(board, 2)
+print(castling_king_side_white2)
+castling_king_side_white3, _ = get_best_moveAB(board, 3)
+print(castling_king_side_white3)
+castling_queen_side_white4, _ = get_best_moveAB(board, 4)
+print(castling_queen_side_white4)
+
 
 # KingSide White Castle
 pboard = [['_' '_' '_' '_' '_' '_' '_' '_'],
@@ -69,7 +105,7 @@ pboard = [['K' '_' '_' '_' 'k' '_' '_' 'r'],
 
 print('Castle KingSide Black')
 
-color_to_play = Color.WHITE
+color_to_play = Color.BLACK
 castling_available = np.array([[False,  False], 
                             [False, True]])
 board = Board()
@@ -83,7 +119,6 @@ castling_queen_side_white3, _ = get_best_moveAB(board, 3)
 print(castling_queen_side_white3)
 castling_queen_side_white4, _ = get_best_moveAB(board, 4)
 print(castling_queen_side_white4)
-
 
 # QueenSide Black Castle
 pboard = [['r' '_' '_' '_' 'k' '_' '_' 'K'],
